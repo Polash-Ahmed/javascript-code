0ba3e5afaefd9f4a113692ac25ddca32a2642416
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-09-17 10:19:17 +0600

    Update script.js
    continue loop added

--- script.js
+++ script.js
@@ -41,3 +41,13 @@ for (var i = 1; i < 10; i++) {
     console.log(i);
   }
 }
+
+
+// Continue Statement
+
+for (var i = 0; i < 10; i++){
+  if(i=== 3 || i === 7){
+    continue
+  }
+  console.log(i)
+}
