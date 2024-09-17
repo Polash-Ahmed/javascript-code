43920d6b4cbf54824468dba4be7d5c352fdb8446
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-09-17 15:07:30 +0600

    Update script.js
    String length added

--- script.js
+++ script.js
@@ -119,4 +119,19 @@ console.log('    kjhgsa  '.trim())
 console.log(c.split(' '))
 
 
+// How to find String Length
 
+var str = 'some string'
+
+var length = 0
+
+while(true){
+  if(str.charAt(length) == ''){
+    break
+  }else{
+    length++
+  }
+}
+
+console.log(length)
+console.log(str.length)
