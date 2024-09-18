800851471ec221d44b9ab157b85b8ef38b3f41f2
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-09-18 19:34:06 +0600

    Create object.js
    frist commit

---
+++ object.js
@@ -0,0 +1,30 @@
+// OBJECT
+
+// empty obj
+var obj = {}
+obj.x = 10
+
+console.log(obj)
+
+
+// object literal
+var point = {
+  x:10,
+  y:20
+}
+
+point.z = 30
+
+console.log(point)
+
+
+// object constractor
+var obj = Object()
+
+obj.a = 10
+console.log(obj)
+
+var obj2 = new Object()
+obj2.b = 20
+
+console.log(obj2)
