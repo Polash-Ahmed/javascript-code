0146348cdc5c6409f8e33de0a314c92132ffc650
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-09-18 20:41:18 +0600

    Update object.js
    object notation

--- object.js
+++ object.js
@@ -28,3 +28,39 @@ var obj2 = new Object()
 obj2.b = 20
 
 console.log(obj2)
+
+
+// object notaion
+var point = {
+  x: 10,
+  y: 20,
+  z:30
+}
+// dot notation
+// console.log(point.y)
+// console.log(point.z)
+// console.log(point.y+point.z)
+// point.x = 5
+// point.a = 16
+// console.log(point)
+
+
+// array notation
+// console.log(point['x'])
+// console.log(point['y'])
+
+// var show = 'x'
+// console.log(point[show])
+
+// update property
+point['y'] = 100
+var prop = 'z'
+point[prop] = 55
+console.log(point)
+
+// delete property
+point.a = undefined
+console.log(point)
+
+delete point.a
+console.log(point)
