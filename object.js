3982bd401328787eb0df9ac5fc8fba88ab3b674d
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-09-18 22:35:34 +0600

    Update object.js
    obj method

--- object.js
+++ object.js
@@ -98,3 +98,28 @@ for(var i in obj){
   console.log(i)
   console.log(i + ': '+obj[i])
 }
+
+
+// obj methid
+var obj = {
+  x: 40,
+  y: 38,
+  z: 75,
+};
+console.log(Object.keys(obj))
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+var obj2 = obj
+obj2.x =70
+obj2.y = 100
+
+console.log(obj)
+console.log(obj2)
+
+// new object create
+
+var obj2 = Object.assign({}, obj)
+obj2.x = 100
+console.log(obj)
+console.log(obj2)
