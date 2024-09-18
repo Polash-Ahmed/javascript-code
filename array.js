0ab341cef7f60cb631e19ecc8afcf82d80c42f15
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-09-18 11:25:02 +0600

    Update array.js
    arrey method added

--- array.js
+++ array.js
@@ -32,3 +32,18 @@ for(var i = 0;i<arr.length;i++){
 }
 console.log('sum = '+sum1)
 
+
+// Arrey method
+
+var arr = [1,2,3,4,5,6,7,8]
+// arr[3] = 9
+// arr.push(9)
+// arr.unshift(9)
+// arr.splice(3,0,9,10)
+// arr[3] = undefined
+// arr.pop()
+// arr.shift()
+// arr.splice(3,1)
+arr.splice(3,1,44)
+console.log(arr)
+
