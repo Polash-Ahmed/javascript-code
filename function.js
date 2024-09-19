01c46ee13e3876098552d9c9847ec23e8147560c
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-09-19 10:08:15 +0600

    Create function.js
    frist commit

---
+++ function.js
@@ -0,0 +1,26 @@
+// Function in javascript
+// What is function
+// input output processing
+
+// function invoke mane function use kora
+function functionName(x,y,z,d,f) {
+  console.log(d*f)
+  console.log(y+z)
+  console.log(x)
+  console.log('i am a function')
+}
+functionName("hello bangladesh",20,29,2,3)
+
+function add() {
+  var a = 10;
+  var b = 20;
+  console.log(a + b);
+}
+add()
+
+function sub() {
+  var a = 30;
+  var b = 20;
+  console.log(a - b);
+}
+sub()
