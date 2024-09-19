a7280c3d9002600f874c6fa8f63ed3b3fb39dcda
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-09-19 20:48:14 +0600

    Update function.js
    arguments

--- function.js
+++ function.js
@@ -24,3 +24,48 @@ function sub() {
   console.log(a - b);
 }
 sub()
+
+// Arguments function
+
+// function add(a, b) {
+//   var result = a + b;
+//   console.log(result);
+// }
+// add(10,20)
+// add(500,700)
+
+// var arr1 = [1,2,3]
+// var arr2 = [4,5,6]
+// var arr3 = [7,8,9]
+
+// function sumOfArray(arr){
+// var sum = 0;
+// for(var i = 0; i<arr.length;i++){
+//   sum +=arr[i]
+// }
+// console.log(sum)
+// }
+// sumOfArray(arr1)
+// sumOfArray(arr2)
+// sumOfArray(arr3)
+
+
+// Arguments
+
+function test(){
+  // console.log(arguments)
+  for(var i = 0; i<arguments.length; i++){
+    console.log(arguments[i])
+  }
+}
+test(10, 20, 30)
+
+function addAll(){
+  var sum = 0
+  for (var i = 0; i <arguments.length; i++){
+    sum +=arguments[i]
+  }
+  console.log(sum)
+}
+addAll(1,2,3,4)
+addAll(6,7,9,9)
