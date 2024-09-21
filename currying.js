3c87631748029285edd57ddc7417fc78f935656c
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-09-21 11:10:00 +0600

    Create currying.js
    curring

---
+++ currying.js
@@ -0,0 +1,16 @@
+// Currying
+
+function add(a, b, c) {
+  return a + b + c;
+}
+
+function currying(a){
+  return function (b){
+    return function (c){
+      return a+b+c
+    }
+  }
+}
+
+var result = currying(5)(10)(15)
+console.log(result)
