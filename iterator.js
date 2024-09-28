57eca3ef0de67257cec562d67bead26c380c857d
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-09-28 20:45:17 +0600

    Update iterator.js
    es6 iterator

--- iterator.js
+++ iterator.js
@@ -19,3 +19,22 @@ console.log(iterate.next())
 console.log(iterate.next())
 console.log(iterate.next())
 console.log(iterate.next())
+
+// es6 iterator
+
+const arr = [1,2,4]
+
+let iterate = arr[Symbol.iterator]()
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+
+let str = 'TEXT'
+let itereteText = str[Symbol.iterator]()
+
+console.log(itereteText.next())
+console.log(itereteText.next())
+console.log(itereteText.next())
+console.log(itereteText.next())
+console.log(itereteText.next())
