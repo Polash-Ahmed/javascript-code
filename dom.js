1ba76b7fbafd03cd337c2bfcb4a2447fd65209ed
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-10-01 10:59:19 +0600

    Update dom.js
    update

--- dom.js
+++ dom.js
@@ -28,3 +28,49 @@ function createElement(tagName, className,innerHTML){
 function append(parent,children){
 children.forEach(child => parent.appendChild(child))
 }
+
+
+
+
+
+
+// update dom class tag...
+
+let li = createElement("li", "list group-item", "four");
+li.setAttribute("title", "i am fourth item");
+let list = document.getElementById("list");
+list.appendChild(li);
+
+// let p1 = createElement('p','lead','hello this is polash ahmed')
+// let p2 = createElement('p','lead','hello this is polash ahmed')
+
+// let div = createElement('div')
+// append(div,[p1,p2])
+
+// list.insertAdjacentElement('afterend',div)
+
+// let container = document.getElementById('cont');
+// container.appendChild(div)
+
+function createElement(tagName, className, innerHTML) {
+  let tag = document.createElement(tagName);
+  tag.innerHTML = innerHTML || "";
+  tag.className = className || "";
+  return tag;
+}
+
+function append(parent, children) {
+  children.forEach((child) => parent.appendChild(child));
+}
+
+let fristChild = list.firstElementChild;
+
+setTimeout(() => {
+  fristChild.innerHTML = fristChild.innerHTML + " (Modified)";
+  fristChild.classList.add("text-success");
+}, 5000);
+
+setTimeout(() => {
+  list.lastChild.remove();
+}, 3000);
+
