ea876692281d471bf2399507cdec5290e556e9be
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-10-04 19:39:47 +0600

    Update reduce.js
    advance reduce

--- reduce.js
+++ reduce.js
@@ -43,3 +43,16 @@ var min = myReduce(
 );
 
 console.log(sum, max, min);
+
+
+// advance reduce
+
+const result = numbers.reduce((acc,cur,index)=>{
+  if(index === 0) acc += '[';
+  if(cur){
+    acc += cur.toString()+ (index < numbers.length-1 ? ', ':'');
+  }
+  if(index===numbers.length) acc+= ']';
+  return acc;
+}, '')
+console.log(result)
