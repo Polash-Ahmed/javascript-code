72f3263fa963ee7cfab70e3864541080d61d0391
Polash Ahmed <134755739+polash99y@users.noreply.github.com>
2024-10-04 16:39:31 +0600

    Update time-menajment.js
    unshift menajment

--- time-menajment.js
+++ time-menajment.js
@@ -21,3 +21,33 @@ console.timeEnd("find");
 console.time("obj");
 arrToObj[id].value = 999;
 console.timeEnd("obj");
+
+// unshift
+
+const arr = [];
+const arrToObj = {};
+for (let i = 0; i < 5000000; i++) {
+  const o = {
+    id: i,
+    value: i,
+  };
+
+  arr.push(o);
+  arrToObj[i] = o;
+}
+
+// object time perfarmence
+console.time("array");
+arr.unshift({
+  id: 5000000,
+  value: 5000000,
+})
+console.timeEnd("array");
+
+
+console.time("obj");
+arrToObj[5000000] = {
+  id: 5000000,
+  value: 5000000,
+}
+console.timeEnd("obj");
